a9a276010d2923f9509a66f164474233bf0b95bb
BassTester <maciej.ubas@gmail.com>
2016-07-07 12:27:45 +0000

    Changed bar style and modified battery warning script

--- .scripts/batterywarning.sh
+++ .scripts/batterywarning.sh
@@ -2,9 +2,25 @@
 BATPATH=/sys/class/power_supply/BAT0
 BAT_FULL=$BATPATH/charge_full
 BAT_NOW=$BATPATH/charge_now
+
+status="$(acpi -i | grep Charging)"
 bf=$(cat $BAT_FULL)
 bn=$(cat $BAT_NOW)
-if [ $(( 100 * $bn / $bf )) -lt 15 ]
-then
-    notify-send "low battery!"
+
+if [ -z $status ]; then
+  if [ $(( 100 * $bn / $bf )) -lt 5 ]; then
+    notify-send "Shutting down in 60 sec"
+    sleep 60
+    systemctl poweroff
+  else
+    if [ $(( 100 * $bn / $bf )) -lt 8 ]; then
+      res=$(echo "shutdown|ignore" | rofi -sep "|" -dmenu -i -p 'Low battery!: ' "" -auto-select)
+      if [ $res = "shutdown" ]; then
+          systemctl poweroff
+      fi
+      if [ $res = "ignore" ]; then
+        echo "ignored"
+      fi
+    fi
+  fi
 fi
